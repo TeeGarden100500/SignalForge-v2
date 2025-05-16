023c545d9a49e033d959031b80d5040de8ad17f3
TeeGarden100500 <hexus1984ab@gmail.com>
2025-05-17 02:01:30 +0400

    Update signalScoring.js

--- logic/signalScoring.js
+++ logic/signalScoring.js
@@ -10,28 +10,3 @@ function getSignalStrength(matchedConditionsCount, totalConditionsCount) {
 module.exports = {
   getSignalStrength
 };
-
-// обновление в comboSignalEngine.js
-const config = require('../config/config');
-const { logInfo } = require('../utils/logger');
-const strategies = require('../strategies/comboStrategies');
-
-function evaluateComboStrategies(context) {
-  strategies.forEach(strategy => {
-    const matched = strategy.conditions.filter(cond => context.conditions.includes(cond));
-    const strength = getSignalStrength(matched.length, strategy.conditions.length);
-
-    if (matched.length === strategy.conditions.length) {
-      const msg = `📢 [${strategy.name}] ${strategy.message} | ${context.symbol} | TF: ${context.timeframe} | Сила: ${strength}`;
-      logInfo(`[comboLog] ${msg}`);
-
-      if (config.ENABLE_WEBHOOK) {
-        // TODO: отправка webhook-сообщения
-      }
-    }
-  });
-}
-
-module.exports = {
-  evaluateComboStrategies
-};
