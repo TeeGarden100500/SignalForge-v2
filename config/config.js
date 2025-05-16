780c8fcfc6df36fcdef694ee99803bebe9a042e4
TeeGarden100500 <hexus1984ab@gmail.com>
2025-05-16 17:59:29 +0400

    Update config.js

--- config/config.js
+++ config/config.js
@@ -37,28 +37,3 @@ module.exports = {
   PERCENT_TO_LOW: 10,
   SUBSCRIPTION_REFRESH_INTERVAL_MS: 5 * 60 * 1000,
 };
-
-// utils/logger.js
-const config = require('../config/config');
-
-function logInfo(message) {
-  if (['basic', 'verbose'].includes(config.DEBUG_LOG_LEVEL)) {
-    console.log(`[INFO] ${new Date().toISOString()} - ${message}`);
-  }
-}
-
-function logVerbose(message) {
-  if (config.DEBUG_LOG_LEVEL === 'verbose') {
-    console.log(`[VERBOSE] ${new Date().toISOString()} - ${message}`);
-  }
-}
-
-function logError(message) {
-  console.error(`[ERROR] ${new Date().toISOString()} - ${message}`);
-}
-
-module.exports = {
-  logInfo,
-  logVerbose,
-  logError
-};
