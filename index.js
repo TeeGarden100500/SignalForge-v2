edcf6de5359aabde997d6938263197caabae28e3
TeeGarden100500 <hexus1984ab@gmail.com>
2025-05-18 01:01:56 +0400

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 const { getTopVolatilePairs } = require('./volatilitySelector');
+const { VOLATILITY_UPDATE_INTERVAL_HOURS } = require('./config');
 
-(async () => {
-  await getTopVolatilePairs();
-})();
+async function runVolatilityScanLoop() {
+  await getTopVolatilePairs(); // первый запуск сразу
+
+  const intervalMs = VOLATILITY_UPDATE_INTERVAL_HOURS * 60 * 60 * 1000;
+  setInterval(async () => {
+    console.log(`\n🔁 Обновление списка волатильных пар...`);
+    await getTopVolatilePairs();
+  }, intervalMs);
+}
+
+runVolatilityScanLoop();
