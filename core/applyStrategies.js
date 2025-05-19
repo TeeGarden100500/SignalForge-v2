7389dda700e5c2c4720c52c769f89a282ee17136
TeeGarden100500 <hexus1984ab@gmail.com>
2025-05-19 20:56:56 +0400

    Create applyStrategies.js

---
+++ core/applyStrategies.js
@@ -0,0 +1,48 @@
+const {
+  checkRSIStrategy,
+  checkMACDStrategy,
+  checkVolumeSpikeStrategy,
+  checkEMACrossoverStrategy,
+  checkEMAAngleStrategy,
+  checkBreakoutStrategy,
+  checkHighLowProximity,
+  checkMeanReversionStrategy,
+  checkATRSpikeStrategy,
+  checkADXStrengthStrategy,
+  checkFiboProximityStrategy,
+  checkDojiPattern // если появится
+} = require('./allStrategies'); // можно объединить импорты
+
+function applyStrategies(symbol, candles, interval) {
+  const signalTags = [];
+  const messages = [];
+  const results = [];
+
+  const add = (res, fallbackTag) => {
+    if (res) {
+      signalTags.push(res.strategy || fallbackTag);
+      messages.push(res.message);
+      results.push(res);
+    }
+  };
+
+  add(checkRSIStrategy(symbol, candles), 'RSI_OVERBOUGHT');
+  add(checkMACDStrategy(symbol, candles, interval), 'MACD');
+  add(checkVolumeSpikeStrategy(symbol, candles, interval), 'VOLUME_SPIKE');
+  add(checkEMACrossoverStrategy(symbol, candles, interval), 'EMA_CROSS');
+  add(checkEMAAngleStrategy(symbol, candles, interval), 'EMA_ANGLE');
+  add(checkBreakoutStrategy(symbol, candles, interval), 'BREAKOUT');
+
+  add(checkHighLowProximity(symbol, candles, interval, 'strict'), 'PROX_HIGH');
+  add(checkHighLowProximity(symbol, candles, interval, 'loose'), 'PROX_HIGH_L');
+
+  add(checkMeanReversionStrategy(symbol, candles, interval), 'MEAN_REVERS_UP');
+  add(checkATRSpikeStrategy(symbol, candles, interval), 'ATR_SPIKE');
+  add(checkADXStrengthStrategy(symbol, candles, interval), 'ADX_TREND');
+  add(checkFiboProximityStrategy(symbol, candles, interval), 'FIBO_TOUCH');
+  // add(checkDojiPattern(candles), 'DOJI');
+
+  return { signalTags, messages, results };
+}
+
+module.exports = { applyStrategies };
