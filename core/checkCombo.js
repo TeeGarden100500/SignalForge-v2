2d9f7c910509a514c527d11ff7bc4d2f570ac168
TeeGarden100500 <hexus1984ab@gmail.com>
2025-05-19 13:26:08 +0400

    Create checkCombo.js

---
+++ core/checkCombo.js
@@ -0,0 +1,23 @@
+const { comboStrategies } = require('./comboStrategies');
+
+function checkComboStrategies(symbol, signals) {
+  const fired = [];
+
+  for (const combo of comboStrategies) {
+    const matched = combo.conditions.every(cond => signals.includes(cond));
+    if (matched) {
+      fired.push({
+        symbol,
+        name: combo.name,
+        message: combo.message,
+        direction: combo.direction
+      });
+    }
+  }
+
+  return fired;
+}
+
+module.exports = {
+  checkComboStrategies
+};
