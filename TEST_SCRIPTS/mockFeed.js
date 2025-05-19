475b306f0e333e8bd9de94c540cd10f3981a839a
TeeGarden100500 <hexus1984ab@gmail.com>
2025-05-19 21:40:59 +0400

    Update mockFeed.js

--- TEST_SCRIPTS/mockFeed.js
+++ TEST_SCRIPTS/mockFeed.js
@@ -1,4 +1,5 @@
 const { applyStrategies } = require('../core/applyStrategies');
+const { checkComboStrategies } = require('../core/checkCombo');
 const mock1 = require('./testData/mock_candles_converted.json');
 const mock2 = require('./testData/mock_atr_spike.json');
 const mock3 = require('./testData/mock_adx_trend_flat.json');
