cc87ecac979cb07d8737b130fea88cc389c9b8d6
TeeGarden100500 <hexus1984ab@gmail.com>
2025-05-19 21:33:43 +0400

    Update mockFeed.js

--- TEST_SCRIPTS/mockFeed.js
+++ TEST_SCRIPTS/mockFeed.js
@@ -1,4 +1,21 @@
 const { applyStrategies } = require('../core/applyStrategies');
+const mock1 = require('./testData/mock_candles_converted.json');
+const mock2 = require('./testData/mock_atr_spike.json');
+const mock3 = require('./testData/mock_adx_trend_flat.json');
+const mock4 = require('./testData/mock_fibo_touch.json');
+const mock5 = require('./testData/mock_combo_momentum.json');
+const mock6 = require('./testData/mock_ema_debug.json');
+
+const mockData = {
+  ...mock1,
+  ...mock2,
+  ...mock3,
+  ...mock4,
+  ...mock5,
+  ...mock6,  
+};
+
+
 
 Object.entries(mockData).forEach(([key, candles]) => {
   const [symbol, interval] = key.split('_');
