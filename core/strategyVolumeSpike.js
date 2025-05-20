9e82d86d477ad0440f4ac9c93bec33b5d41625dd
TeeGarden100500 <hexus1984ab@gmail.com>
2025-05-20 21:56:35 +0400

    Update strategyVolumeSpike.js

--- core/strategyVolumeSpike.js
+++ core/strategyVolumeSpike.js
@@ -1,8 +1,18 @@
-const { detectVolumeSpike } = require('./indicators');
-
 function checkVolumeSpikeStrategy(symbol, candles, interval) {
   const result = detectVolumeSpike(candles);
   if (!result || !result.spike) return null;
+
+  const volume = candles.at(-1).volume;
+  const prevVolume = result.avgVolume;
+
+  if (volume < prevVolume * 0.5) {
+    return {
+      symbol,
+      strategy: 'VOLUME_DROP',
+      tag: 'VOLUME_DROP',
+      message: `📉 [${symbol}] Объём резко снизился: ${volume.toFixed(2)} < ${prevVolume.toFixed(2)}`
+    };
+  }
 
   return {
     symbol,
@@ -10,16 +20,8 @@ function checkVolumeSpikeStrategy(symbol, candles, interval) {
     tag: 'VOLUME_SPIKE',
     message: `⚡ [${symbol}] Объём выше нормы в ${result.ratio}× (${result.volume} против среднего ${result.avgVolume})`
   };
-  
-  if (volume < prevVolume * 0.5) {
-  return {
-    symbol,
-    strategy: 'VOLUME_DROP',
-    tag: 'VOLUME_DROP',
-    message: `📉 [${symbol}] Объём резко снизился: ${volume.toFixed(2)} < ${prevVolume.toFixed(2)}`
-  };
 }
- 
+
 module.exports = {
   checkVolumeSpikeStrategy
 };
