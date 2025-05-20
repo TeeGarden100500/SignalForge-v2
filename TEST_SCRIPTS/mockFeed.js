2339627c59c307234439946f151db2964e5381aa
TeeGarden100500 <hexus1984ab@gmail.com>
2025-05-20 12:42:54 +0400

    Update mockFeed.js

--- TEST_SCRIPTS/mockFeed.js
+++ TEST_SCRIPTS/mockFeed.js
@@ -1,11 +1,11 @@
 const { applyStrategies } = require('../core/applyStrategies');
 const { checkComboStrategies } = require('../core/checkCombo');
-const mock1 = require('./testData/mock_candles_converted.json');
-const mock2 = require('./testData/mock_atr_spike.json');
-const mock3 = require('./testData/mock_adx_trend_flat.json');
-const mock4 = require('./testData/mock_fibo_touch.json');
-const mock5 = require('./testData/mock_combo_momentum.json');
-const mock6 = require('./testData/mock_ema_debug.json');
+//const mock1 = require('./testData/mock_candles_converted.json');
+//const mock2 = require('./testData/mock_atr_spike.json');
+//const mock3 = require('./testData/mock_adx_trend_flat.json');
+//const mock4 = require('./testData/mock_fibo_touch.json');
+//const mock5 = require('./testData/mock_combo_momentum.json');
+//const mock6 = require('./testData/mock_ema_debug.json');
 const mock7 = require('./testData/mock_falling_knife.json');
 const mock8 = require('./testData/mock_exhaustion_top.json');
 const mock9 = require('./testData/mock_bullish_divergence.json');
