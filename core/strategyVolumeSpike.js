3140460c7f70f0f190e173b8ebf9c332ba4ca17f
TeeGarden100500 <hexus1984ab@gmail.com>
2025-05-20 19:11:21 +0400

    Update strategyVolumeSpike.js

--- core/strategyVolumeSpike.js
+++ core/strategyVolumeSpike.js
@@ -10,6 +10,14 @@ function checkVolumeSpikeStrategy(symbol, candles, interval) {
     tag: 'VOLUME_SPIKE',
     message: `⚡ [${symbol}] Объём выше нормы в ${result.ratio}× (${result.volume} против среднего ${result.avgVolume})`
   };
+  
+  if (volume < avgVolume * 0.5) {
+  return {
+    symbol,
+    strategy: 'VOLUME_LOW',
+    tag: 'VOLUME_LOW',
+    message: `💤 [${symbol}] Объём ниже нормы: ${volume.toFixed(2)} против среднего ${avgVolume.toFixed(2)}`
+  };
 }
 
 module.exports = {
