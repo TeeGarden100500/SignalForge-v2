9bf2a3eca5c0bf683e3486b7b06b97e3bd08b052
TeeGarden100500 <hexus1984ab@gmail.com>
2025-05-21 07:37:49 +0400

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  TOP_N_PAIRS: 5,
+  TOP_N_PAIRS: 50,
   PAIR_SUFFIX: 'USDT',
   DEBUG_LOG_LEVEL: 'none', // 'none' | 'basic' | 'verbose'
   VOLATILITY_UPDATE_INTERVAL_HOURS: 6, // обновление каждые 6 часов после тестов поставить цифру 6
