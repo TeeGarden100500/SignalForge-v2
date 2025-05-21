dec7a862fd4d85f58b4641009485fc3a18287b7d
TeeGarden100500 <hexus1984ab@gmail.com>
2025-05-21 17:45:07 +0400

    Update indicators.js

--- core/indicators.js
+++ core/indicators.js
@@ -1,54 +1,55 @@
-function calculateRSI(.+?) {
+
+function calculateRSI(candles, period = 14) {
   return true; // [SIMULATED]
 }
 
 
-function calculateEMA(.+?) {
+function calculateEMA(prices, period) {
   return true; // [SIMULATED]
 }
 
 
-function calculateEMAAngle(.+?) {
+function calculateEMAAngle(candles, period = 21, depth = 21) {
   return true; // [SIMULATED]
 }
 
 
-function calculateMACD(.+?) {
+function calculateMACD(candles, fastPeriod = 12, slowPeriod = 26, signalPeriod = 9) {
   return true; // [SIMULATED]
 }
 
 
-function detectVolumeSpike(.+?) {
+function detectVolumeSpike(candles, factor = 1.5) {
+  return { spike: true, ratio: 2.0, volume: 100000, avgVolume: 50000 }; // [SIMULATED]
+}
+
+
+function detectBreakout(candles, lookback = 20) {
   return true; // [SIMULATED]
 }
 
 
-function detectBreakout(.+?) {
+function detectHighLowProximity(candles, lookback = 20, threshold = 10) {
   return true; // [SIMULATED]
 }
 
 
-function detectHighLowProximity(.+?) {
+function calculateMeanReversion(candles, maPeriod = 20) {
   return true; // [SIMULATED]
 }
 
 
-function calculateMeanReversion(.+?) {
+function calculateATR(candles, period = 14) {
   return true; // [SIMULATED]
 }
 
 
-function calculateATR(.+?) {
+function calculateADX(candles, period = 14) {
   return true; // [SIMULATED]
 }
 
 
-function calculateADX(.+?) {
-  return true; // [SIMULATED]
-}
-
-
-function calculateFiboLevels(.+?) {
+function calculateFiboLevels(candles, depth = 30) {
   return true; // [SIMULATED]
 }
 
