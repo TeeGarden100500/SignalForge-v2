c05e76a4ff148148ac7814197fad500f30daf2b7
TeeGarden100500 <hexus1984ab@gmail.com>
2025-05-22 14:59:19 +0400

    Create analyzeSymbol.js

---
+++ analyzeSymbol.js
@@ -0,0 +1,16 @@
+// src/analyzeSymbol.js
+
+const { applyStrategies } = require('./applyStrategies');
+const { sendWebhook } = require('../webhookHandler');
+const { getCandlesFor } = require('../cache/candleStore'); // или твой источник
+
+function analyzeSymbol(symbol, interval) {
+  const candles = getCandlesFor(symbol, interval); // получаем свечи
+  const results = applyStrategies(symbol, candles, interval); // применяем стратегии
+
+  if (results.length > 0) {
+    sendWebhook(symbol, results, interval); // отправляем сигнал
+  }
+}
+
+module.exports = { analyzeSymbol };
