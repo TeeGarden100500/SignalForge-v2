9fd56962416ae1aa0e4bc21291d57f282ff58d49
TeeGarden100500 <hexus1984ab@gmail.com>
2025-05-23 12:07:05 +0400

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 module.exports = {
-  TOP_N_PAIRS: 50,
+  TOP_N_PAIRS: 10,
   PAIR_SUFFIX: 'USDT',
   SYMBOL_ANALYSIS_DELAY_MS: 500, // 0.5 сек между монетами (на 50 монет ≈ 25 сек)
-  DEBUG_LOG_LEVEL: 'none', // 'none' | 'basic' | 'verbose'
+  DEBUG_LOG_LEVEL: 'verbose', // 'none' | 'basic' | 'verbose'
   VOLATILITY_UPDATE_INTERVAL_HOURS: 6, // обновление каждые 6 часов после тестов поставить цифру 6
   CACHE_LIMITS: {
   '5m': 120,
