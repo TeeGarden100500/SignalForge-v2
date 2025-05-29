f568fc6dc5d0a3e6ed05c4d21cc91594564cf93b
TeeGarden100500 <hexus1984ab@gmail.com>
2025-05-29 18:22:26 +0400

    Update cacheSaver.js

--- cache/cacheSaver.js
+++ cache/cacheSaver.js
@@ -5,7 +5,7 @@ function saveCacheToFile(cache, filename = 'candles_cache.json') {
   const filePath = path.join(__dirname, filename);
   try {
     fs.writeFileSync(filePath, JSON.stringify(cache, null, 2));
-    console.log(`[CACHE] ✅ Кэш свечей сохранён в ${filePath}`);
+//    console.log(`[CACHE] ✅ Кэш свечей сохранён в ${filePath}`);
   } catch (err) {
     console.error(`[CACHE] ❌ Ошибка при сохранении кэша:`, err);
   }
