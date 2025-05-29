3c80dc77758c8e041e90a0e97559bba7754a5c14
TeeGarden100500 <hexus1984ab@gmail.com>
2025-05-29 19:44:33 +0400

    Update config.js

--- config.js
+++ config.js
@@ -8,9 +8,9 @@ module.exports = {
   DOJI_BODY_RATIO: 0.3, // 🔧 Условие для Doji: тело составляет менее 30% от полного диапазона свечи
 
   GITHUB_CACHE_ENABLED: true,
-  GITHUB_TOKEN: 'ghp_xxxxxxxxxxxxxxxxxxxxx', // токен GitHub
-  GIST_ID: 'abc1234567890abcdef...',          // ID твоего Gist
-  GIST_FILENAME: 'candles_cache.json'
+  GITHUB_TOKEN: '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', // токен GitHub
+  GIST_ID: '8d44a82bf2917e2560b5e1ed6eb9a831', // ID твоего Gist
+  GIST_FILENAME: 'candles_cache.json',
  
                                           // === RSI Settings ===
   //**************************************************************************************************************************
