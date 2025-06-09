292ad6bef9659f0186cdd3357735ff5665b20dc0
TeeGarden100500 <hexus1984ab@gmail.com>
2025-06-09 19:58:03 +0400

    Add candle length checks for key strategies

---
+++ utils/candleValidator.js
@@ -0,0 +1,29 @@
+const { DEBUG_LOG_LEVEL } = require('../config');
+
+let skippedStrategies = 0;
+
+function hasEnoughCandles(candles, minRequired, strategyName) {
+  const length = Array.isArray(candles) ? candles.length : 0;
+  if (length < minRequired) {
+    if (DEBUG_LOG_LEVEL === 'verbose') {
+      console.warn(`[SKIP] ${strategyName}: недостаточно данных (${length}/${minRequired})`);
+    }
+    skippedStrategies++;
+    return false;
+  }
+  return true;
+}
+
+function getSkippedStrategies() {
+  return skippedStrategies;
+}
+
+function resetSkippedStrategies() {
+  skippedStrategies = 0;
+}
+
+module.exports = {
+  hasEnoughCandles,
+  getSkippedStrategies,
+  resetSkippedStrategies,
+};
